46bb6b107749721be537c6ce2cfe884aff3ee676
unknown <facubazan992009@gmail.com>
2025-09-25 19:27:33 -0300

    creacion de rutas y controladores para los empleados

--- backEnd/routes/route.empleados.js
+++ backEnd/routes/route.empleados.js
@@ -1,9 +1,13 @@
 const express = require("express")
-const {getEmpleados}=require("../controllers/empleados")
+const {getEmpleados, createEmpleado, updateEmpleado, darBajaEmpleado, reactivarEmpleado}=require("../controllers/empleados")
 
 const router=express.Router()
 
 router.get("/empleados",getEmpleados)
+router.post("/empleados/crear",createEmpleado)
+router.put("/empleados/actualizar/:id", updateEmpleado)
+router.put("/empleados/baja/:id",darBajaEmpleado)
+router.put("/empleados/reactivar/:id",reactivarEmpleado)
 
 
 module.exports=router
