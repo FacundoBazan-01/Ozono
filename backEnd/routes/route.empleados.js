35b6409f72626afaaa5c37931c0c35f0b0fa3b24
unknown <facubazan992009@gmail.com>
2025-09-30 19:24:38 -0300

    agrego carpeta middlewars

--- backEnd/routes/route.empleados.js
+++ backEnd/routes/route.empleados.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const {getEmpleados, createEmpleado, updateEmpleado, darBajaEmpleado, reactivarEmpleado}=require("../controllers/empleados")
+const auth = require("../middlewars/auth")
 
 const router=express.Router()
 
-router.get("/empleados",getEmpleados)
+router.get("/empleados",auth,getEmpleados)
 router.post("/empleados/crear",createEmpleado)
 router.put("/empleados/actualizar/:id", updateEmpleado)
 router.put("/empleados/baja/:id",darBajaEmpleado)
